44d26ac47605c829cb7dff63a4cd1351840f06d9
timEU <timeu.fo@gmail.com>
2022-08-16 11:58:13 -0300

    adicionando flask

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import requests
+import os
+from flask import Flask, request, jsonify, json
+
 API_KEY = '2663e2bbd329aa8ccb641c1b9e05ae98'
 
-cidade = input('Digite o nome da cidade: ')
+app = Flask(__name__)
 
-link = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric&lang=pt_br'
+@app.route("/<cidade>")
 
-requisicao = requests.get(link)
-result = requisicao.json()
-descricao = result['weather'][0]['description']
-temp_atual = result['main']['temp']
-temp_max = result['main']['temp_max']
-temp_min = result['main']['temp_min']
-real_feel = result['main']['feels_like']
-print(f"Nuvens: {descricao}")
-print(f"Temperatura atual: {temp_atual}")
-print(f"Temperatura atual: {temp_max}")
-print(f"Temperatura atual: {temp_min}")
-print(f"Temperatura atual: {real_feel}")
+def mostra_temp(cidade):
+    #cidade = input('Digite o nome da cidade: ')
+    link = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric&lang=pt_br'
+    requisicao = requests.get(link)
+    result = requisicao.json()
+    descricao = result['weather'][0]['description']
+    temp_atual = result['main']['temp']
+    temp_max = result['main']['temp_max']
+    temp_min = result['main']['temp_min']
+    real_feel = result['main']['feels_like']
+    #print(f"Nuvens: {descricao}")
+    #print(f"Temperatura atual: {temp_atual}")
+    #print(f"Temperatura maxima: {temp_max}")
+    #print(f"Temperatura minima: {temp_min}")
+    #print(f"Sensação termica: {real_feel}")
+    return f"Nuvens: {result['weather'][0]['description']}"
+
+app.run(host="0.0.0.0", port=2000, debug = False)
