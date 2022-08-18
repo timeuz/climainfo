bc8d3f249a02666a883b8575902458b3ab0a4020
timeuz <timeu.fo@gmail.com>
2022-08-17 23:19:21 -0300

    criação do dict e próximos passos anotados

--- main.py
+++ main.py
@@ -12,17 +12,14 @@ def mostra_temp(cidade):
     #cidade = input('Digite o nome da cidade: ')
     link = f'https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric&lang=pt_br'
     requisicao = requests.get(link)
-    result = requisicao.json()
-    descricao = result['weather'][0]['description']
-    temp_atual = result['main']['temp']
-    temp_max = result['main']['temp_max']
-    temp_min = result['main']['temp_min']
-    real_feel = result['main']['feels_like']
-    #print(f"Nuvens: {descricao}")
-    #print(f"Temperatura atual: {temp_atual}")
-    #print(f"Temperatura maxima: {temp_max}")
-    #print(f"Temperatura minima: {temp_min}")
-    #print(f"Sensação termica: {real_feel}")
-    return f"Nuvens: {result['weather'][0]['description']}"
+    result = json.loads(requisicao.text)
+    teste = {
+        "descricao" : result['weather'][0]['description'],
+        "temp_atual" : result['main']['temp'],
+        "temp_max" : result['main']['temp_max'],
+        "temp_min" : result['main']['temp_min'],
+        "real_feel" : result['main']['feels_like']
+    }
+    return teste
 
-app.run(host="0.0.0.0", port=2000, debug = False)
+app.run(host="0.0.0.0", port=2000, debug = True)
